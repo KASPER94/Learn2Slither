29d2fbeaad2caed9660446dff35e9ba4188e0465
Simon Kaperski <skapersk@bess-f4r2s4.clusters.42paris.fr>
2025-02-28 17:30:03 +0100

    snake working

--- README.md
+++ README.md
@@ -1,11 +1,32 @@
 # Learn2Slither
 
 42 set up:
-    $> MYPATH="/goinfre/$USER/miniconda3"
-    $> curl -LO "https://repo.anaconda.com/miniconda/ Miniconda3-latest-Linux-x86_64.sh"
-    $>sh Miniconda3-latest-Linux-x86_64.sh -b -p $MYPATH
-    $> $MYPATH/bin/conda init bash
-    $> $MYPATH/bin/conda config --set auto_activate_base false
-    $> source ~/.bash_profile
-    $> conda create --name 42AI-$USER python=3.7 jupyter pandas pycodestyle numpy pygame
-    $> conda activate 42AI-skapersk
+    # 1. Installer Miniconda
+    MYPATH="/goinfre/$USER/miniconda3"
+    curl -LO "https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh"
+    sh Miniconda3-latest-Linux-x86_64.sh -b -p $MYPATH
+    $MYPATH/bin/conda init bash
+    $MYPATH/bin/conda config --set auto_activate_base false
+    source ~/.bashrc
+
+    # 2. Créer l'environnement Conda
+    conda create --name 42AI-$USER python=3.7 jupyter pandas pycodestyle numpy pygame -y
+    conda activate 42AI-$USER
+
+    # 3. Copier la bonne version de libstdc++.so.6 dans Conda
+    cp /usr/lib/x86_64-linux-gnu/libstdc++.so.6* /goinfre/$USER/miniconda3/envs/42AI-$USER/lib/
+
+    # 4. Ajouter les variables d'environnement au .bashrc
+    echo 'export LD_LIBRARY_PATH=/goinfre/$USER/miniconda3/envs/42AI-$USER/lib:$LD_LIBRARY_PATH' >> ~/.bashrc
+    echo 'export SDL_VIDEODRIVER=x11' >> ~/.bashrc
+    echo 'conda activate 42AI-$USER' >> ~/.bashrc
+    source ~/.bashrc
+
+    # 5. Vérifier que tout fonctionne
+    python3 -c "import pygame; print('Pygame OK')"
+    echo $LD_LIBRARY_PATH
+    echo $SDL_VIDEODRIVER
+    conda info --envs
+
+    # 6. Tester Learn2Slither
+    python3 test.py
--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import numpy as np
 import pygame as pg
 import random
+import os
+os.environ["SDL_VIDEODRIVER"] = "x11"  
+os.environ["SDL_RENDER_DRIVER"] = "software" 
+os.environ["LIBGL_ALWAYS_SOFTWARE"] = "1" 
+os.environ["MESA_LOADER_DRIVER_OVERRIDE"] = "swrast"
 
 GRID_SIZE = 10
 CELL_SIZE = 40
@@ -13,7 +18,9 @@ RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 
 class SnakeEnv:
-    def __init__(self, size):
+    def __init__(self, size=GRID_SIZE):
+        self.dir = "RIGTH"
+        self.started = False
         self.size = size
         self.reset()
 
@@ -37,26 +44,82 @@ class SnakeEnv:
         self.board[self.green_apples] = 2
         self.board[self.red_apples] = 3
 
+    def update_snake(self):
+        """Move snake"""
+        if not self.started or self.dir is None:
+            return
+        head_x, head_y = self.snake[0]
+        new_head = (0, 0)
+        if self.dir == "UP":
+            new_head = (head_x - 1, head_y)
+        elif self.dir == "DOWN":
+            new_head = (head_x + 1, head_y)
+        elif self.dir == "RIGHT":
+            new_head = (head_x, head_y + 1)
+        elif self.dir == "LEFT":
+            new_head = (head_x, head_y - 1)
+
+        if new_head in self.snake or new_head[0] < 0 or new_head[0] >= self.size or new_head[1] >= self.size:
+            print("Game Over")
+            self.reset()
+            return
+        
+        self.snake.insert(0, new_head)
+
+        if new_head == self.green_apples:
+            self.spawn_apples()
+        else:
+            self.snake.pop()
+
+def key_event():
+    keys = pg.key.get_pressed()
+    if keys[pg.K_w] or keys[pg.K_UP]:
+        if snakeEnv.dir != "DOWN":
+            snakeEnv.dir = "UP"
+            snakeEnv.started = True
+    elif keys[pg.K_s] or keys[pg.K_DOWN]:
+        if snakeEnv.dir != "UP":
+            snakeEnv.dir = "DOWN"
+            snakeEnv.started = True
+    elif keys[pg.K_a] or keys[pg.K_LEFT]:
+        if snakeEnv.dir != "RIGHT":
+            snakeEnv.dir = "LEFT"
+            snakeEnv.started = True
+    elif keys[pg.K_d] or keys[pg.K_RIGHT]:
+        if snakeEnv.dir != "LEFT":
+            snakeEnv.dir = "RIGHT"
+            snakeEnv.started = True
 
 
-if __name__ == "__main":
-    snakeEnv = SnakeEnv(10)
+if __name__ == "__main__":
     pg.init()
-    screen = pg.display.set_mode((WINDOW_SIZE * WINDOW_SIZE))
+    snakeEnv = SnakeEnv(10)
+    screen = pg.display.set_mode((WINDOW_SIZE, WINDOW_SIZE))
+    pg.display.set_caption("Learn2Slither - Snake RL")
     clock = pg.time.Clock()
     running = True
-
     while running:
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 running = False
-        screen.fill(WHITE)
+
+        key_event()
+        snakeEnv.update_snake()
+
+        screen.fill(BLACK)
+
         for i in range(0, WINDOW_SIZE, CELL_SIZE):
             for y in range(0, WINDOW_SIZE, CELL_SIZE):
-                rect = pg.draw.Rect(i, y, CELL_SIZE, CELL_SIZE)
-                pg.draw.rect(screen, BLACK, rect, 1)
+                rect = pg.Rect(i, y, CELL_SIZE, CELL_SIZE)
+                pg.draw.rect(screen, WHITE, rect, 1)
         
         pg.draw.rect(screen, GREEN, (snakeEnv.green_apples[1] * CELL_SIZE, snakeEnv.green_apples[0] * CELL_SIZE, CELL_SIZE, CELL_SIZE))
         pg.draw.rect(screen, RED, (snakeEnv.red_apples[1] * CELL_SIZE, snakeEnv.red_apples[0] * CELL_SIZE, CELL_SIZE, CELL_SIZE))
 
+        for seg in snakeEnv.snake:
+            pg.draw.rect(screen, BLUE, (seg[1] * CELL_SIZE, seg[0] * CELL_SIZE, CELL_SIZE, CELL_SIZE))
+        
+
+        pg.display.flip()
         clock.tick(5)
+    pg.quit()
